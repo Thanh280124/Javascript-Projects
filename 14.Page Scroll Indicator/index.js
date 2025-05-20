4f55bb7441b9ca8f76a2f0246033b5e2dd12d158
Pham Thanh <thanhpham28124@gmail.com>
2025-05-20 20:27:08 +0300

    changechange

--- 14.Page Scroll Indicator/index.js
+++ 14.Page Scroll Indicator/index.js
@@ -1,12 +1,11 @@
 let indicator = document.querySelector(".scroll-indicator-bar");
 let scrollHeight = document.body.scrollHeight - document.documentElement.clientHeight;
-console.log(scrollHeight);
+
 
 window.addEventListener("scroll", () => {
     let scrollTop = document.documentElement.scrollTop;
-    console.log(scrollTop);
     
     let scrollPercent = (scrollTop / scrollHeight) * 100;
-    console.log(scrollPercent);
+   
     indicator.style.width = `${scrollPercent}%`;
 });
