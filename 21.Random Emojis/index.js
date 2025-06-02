e00b41d368f85a176a0c517b01c5834b98c9a8fd
Pham Thanh <thanhpham28124@gmail.com>
2025-06-02 13:19:41 +0300

    random emojis

---
+++ 21.Random Emojis/index.js
@@ -0,0 +1,13 @@
+const btn =document.getElementById('emoji');
+
+const emojis = [
+  "😊", "❤️", "😍", "😒", "👌", "😘", "💕", "😁", "👍", "🙌",
+  "😀", "😃", "😄", "😁", "😆", "😊", "☺️", "🙌", "🙋", "🙋‍♂️",
+  "🥲", "😔", "😟", "🥺", "🥹", "😓", "😞", "😖", "😭", "😢", "😥"
+];
+
+btn.addEventListener('mouseover', () =>{
+    btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)]
+})
+
+
