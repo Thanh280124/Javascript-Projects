3734b64fc41e7ca89052d50bf912cca9c2f6779e
Pham Thanh <thanhpham28124@gmail.com>
2025-06-21 19:44:06 +0300

    Interactive Accordion

--- 41.Interactive Accordion/index.js
+++ 41.Interactive Accordion/index.js
@@ -4,13 +4,18 @@ const tabs = document.querySelectorAll(".tab");
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     const target = button.getAttribute("data-tabs");
-    buttons.forEach(btn => btn.classList.remove("active"));
-    // Add active class to the clicked button
+
+    buttons.forEach(btn => {
+      btn.classList.remove("active");
+      btn.querySelector("img").classList.remove("rotate");
+    });
+
+  
     button.classList.add("active");
-    
+    button.querySelector("img").classList.add("rotate");
+
     tabs.forEach(tab => tab.classList.remove("active"));
 
-    // Add active class to the target tab
     document.getElementById(`tab-${target}`).classList.add("active");
   });
 });
