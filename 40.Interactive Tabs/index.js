dd5784e4c9ffeb4ffec93b55dfa67dea3bca981b
Pham Thanh <thanhpham28124@gmail.com>
2025-06-21 09:15:19 +0300

    change name project

---
+++ 40.Interactive Tabs/index.js
@@ -0,0 +1,16 @@
+const buttons = document.querySelectorAll(".tab-buttons button");
+const tabs = document.querySelectorAll(".tab");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const target = button.getAttribute("data-tabs");
+    buttons.forEach(btn => btn.classList.remove("active"));
+    // Add active class to the clicked button
+    button.classList.add("active");
+    
+    tabs.forEach(tab => tab.classList.remove("active"));
+
+    // Add active class to the target tab
+    document.getElementById(`tab-${target}`).classList.add("active");
+  });
+});
